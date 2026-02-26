d7dbcbb808cdd05c820bdea606fac6b78e4aefc5
cacti-lfs <lois.dutour@gastonberger.fr>
2026-02-26 09:57:54 +0100

    add:composant dhcp

---
+++ terraform/composants/02_dhcp/locals.tf
@@ -0,0 +1,17 @@
+locals {
+  node_template_map = {
+    (var.node_name_1) = var.template_id_node1
+    (var.node_name_2) = var.template_id_node2
+  }
+
+    dhcp_vm = {
+    "01" = {
+      ipv4_address = var.ip_dhcp_01
+      node_name = var.node_name_1
+    }
+    "02" = {
+      ipv4_address = var.ip_dhcp_01
+      node_name = var.node_name_1
+    }
+  }
+}
---
+++ terraform/composants/02_dhcp/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    proxmox = {
+      version = "0.94.0"
+      source  = "bpg/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.proxmox_api_url
+  api_token = "${var.proxmox_api_user}!${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
+  insecure  = true
+
+  ssh {
+    agent = true
+  }
+
+}
