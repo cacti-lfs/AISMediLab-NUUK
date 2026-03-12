37e73cc6ae215ba21a834bce24b6c5fb3bdb8a8c
cacti-lfs <lois.dutour@gastonberger.fr>
2026-03-12 13:13:47 +0100

    fix(tf):provider locals

--- terraform/composants/03_databases/locals.tf
+++ terraform/composants/03_databases/locals.tf
@@ -1,9 +1,8 @@
-# locals.tf
 locals {
   node_template_map = {
     (var.node_name_linux_1) = var.template_linux_id_node1
     (var.node_name_linux_2) = var.template_linux_id_node2
-    (var.node_name_linux_2) = var.template_linux_id_node3
+    (var.node_name_linux_3) = var.template_linux_id_node3
   }
 
   bdd_web_vm = {
@@ -11,21 +10,18 @@ locals {
       ipv4_address = var.ip_bdd_web_01
       node_name    = var.node_name_linux_1
       template_id  = var.template_linux_id_node1
-      node_name    = var.node_name_linux_1
       provider     = "provider_node1"
     }
     "02" = {
       ipv4_address = var.ip_bdd_web_02
       node_name    = var.node_name_linux_2
       template_id  = var.template_linux_id_node2
-      node_name    = var.node_name_linux_2
       provider     = "provider_node2"
     }
     "03" = {
       ipv4_address = var.ip_bdd_web_03
       node_name    = var.node_name_linux_3
       template_id  = var.template_linux_id_node3
-      node_name    = var.node_name_linux_3
       provider     = "provider_node3"
     }
   }
@@ -35,21 +31,18 @@ locals {
       ipv4_address = var.ip_bdd_adm_01
       node_name    = var.node_name_linux_1
       template_id  = var.template_linux_id_node1
-      node_name    = var.node_name_linux_1
       provider     = "provider_node1"
     }
     "02" = {
       ipv4_address = var.ip_bdd_adm_02
       node_name    = var.node_name_linux_2
       template_id  = var.template_linux_id_node2
-      node_name    = var.node_name_linux_2
       provider     = "provider_node2"
     }
     "03" = {
       ipv4_address = var.ip_bdd_adm_03
       node_name    = var.node_name_linux_3
       template_id  = var.template_linux_id_node3
-      node_name    = var.node_name_linux_3
       provider     = "provider_node3"
     }
   }
@@ -59,21 +52,18 @@ locals {
       ipv4_address = var.ip_bdd_mon_01
       node_name    = var.node_name_linux_1
       template_id  = var.template_linux_id_node1
-      node_name    = var.node_name_linux_1
       provider     = "provider_node1"
     }
     "02" = {
       ipv4_address = var.ip_bdd_mon_02
       node_name    = var.node_name_linux_2
       template_id  = var.template_linux_id_node2
-      node_name    = var.node_name_linux_2
       provider     = "provider_node2"
     }
     "03" = {
       ipv4_address = var.ip_bdd_mon_03
       node_name    = var.node_name_linux_3
       template_id  = var.template_linux_id_node3
-      node_name    = var.node_name_linux_3
       provider     = "provider_node3"
     }
   }
