ac725a2ce7c55b2edd107820c49470edda8b6fb6
cacti-lfs <lois.dutour@gastonberger.fr>
2026-03-12 11:38:32 +0100

    fix(tf):vars networks

--- terraform/composants/03_databases/variables.tf
+++ terraform/composants/03_databases/variables.tf
@@ -51,14 +51,17 @@ variable "network_v1" {
 variable "gateway_vlan140" {
   type        = string
   description = "VLAN BDD WEB"
+  default = "null"
 }
 variable "gateway_vlan150" {
   type        = string
   description = "VLAN BDD ADM"
+  default = "null"
 }
 variable "gateway_vlan160" {
   type        = string
   description = "VLAN BDD MON"
+  default = "null"
 }
 variable "ip_bdd_web_01" {
   type    = string
@@ -102,15 +105,15 @@ variable "full_clone" {
 }
 variable "ipv4_cidr_bdd_web" {
   type    = string
-  default = "/28"
+  default = null
 }
 variable "ipv4_cidr_bdd_adm" {
   type    = string
-  default = "/28"
+  default = null
 }
 variable "ipv4_cidr_bdd_mon" {
   type    = string
-  default = "/28"
+  default = null
 }
 variable "environnement" {
   type    = string
