c15c2ac3811e938c804bf31db291aa2356eab8bc
cacti-lfs <lois.dutour@gastonberger.fr>
2026-05-01 22:07:10 +0200

    feat(tf): add haproxy

--- terraform/composants/02_admin_zone/provider.tf
+++ terraform/composants/02_admin_zone/provider.tf
@@ -1,15 +1,27 @@
 terraform {
+  required_version = ">=1.5.0"
   required_providers {
     proxmox = {
-      version = "0.98.0"
+      version = ">=0.100.0"
       source  = "bpg/proxmox"
     }
   }
 }
 
 provider "proxmox" {
+  alias     = "provider_node_vip"
+  endpoint  = "https://192.168.32.132:8006"
+  api_token = "${var.proxmox_api_user}!${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
+  insecure  = true
+
+  ssh {
+    agent = true
+  }
+}
+
+provider "proxmox" {
   alias     = "provider_node1"
-  endpoint  = var.proxmox_api_url_node_1
+  endpoint  = "https://192.168.32.132:8006"
   api_token = "${var.proxmox_api_user}!${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
   insecure  = true
 
@@ -20,17 +32,7 @@ provider "proxmox" {
 
 provider "proxmox" {
   alias     = "provider_node2"
-  endpoint  = var.proxmox_api_url_node_2
-  api_token = "${var.proxmox_api_user}!${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
-  insecure  = true
-
-  ssh {
-    agent = true
-  }
-}
-provider "proxmox" {
-  alias     = "provider_node3"
-  endpoint  = var.proxmox_api_url_node_3
+  endpoint  = "https://192.168.32.132:8006"
   api_token = "${var.proxmox_api_user}!${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
   insecure  = true
 
---
+++ terraform/composants/02_admin_zone/variables.tf
@@ -0,0 +1,54 @@
+variable "proxmox_api_token_id" {
+  type      = string
+  sensitive = true
+}
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+variable "proxmox_api_user" {
+  type = string
+}
+variable "ssh_public_keys" {
+  type = list(string)
+}
+variable "node_name_linux_1" {
+  type = string
+}
+variable "node_name_linux_2" {
+  type = string
+}
+variable "node_name_linux_3" {
+  type = string
+}
+variable "template_linux_id" {
+  type    = number
+  default = 9001
+}
+variable "datastore_id" {
+  type = string
+}
+variable "network_v2" {
+  type = string
+}
+variable "gateway_vlan120" {
+  type = string
+}
+variable "full_clone" {
+  type    = bool
+  default = true
+}
+variable "ipv4_cidr_120" {
+  type    = string
+  default = "28"
+}
+variable "environnement" {
+  type    = string
+  default = "nuuk"
+}
+variable "ip_haproxy_1" {
+  type = string
+}
+variable "ip_haproxy_2" {
+  type = string
+}
